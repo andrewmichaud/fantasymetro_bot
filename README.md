a1fb2c3cbf83d10fa931a5a9a7287ac81ce2cde9
Andrew Michaud <dev@mail.andrewmichaud.com>
2016-11-16 19:41:15 -0800

    basic project files

---
+++ README.md
@@ -0,0 +1,4 @@
+# fantasymetro_bot
+bot generating imaginary metro systems
+
+Created by Andrew Michaud for #NaBoMaMo 2016
---
+++ setup.py
@@ -0,0 +1,27 @@
+from os import path
+from setuptools import setup, find_packages
+
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, "VERSION"), encoding="utf-8") as f:
+    VERSION = f.read().strip()
+
+setup(author="Andrew Michaud",
+      author_email="bots+fantasymetro@mail.andrewmichaud.com",
+
+      entry_points={
+          "console_scripts": ["fantasy_metro_bot = fantasy_metro_bot.__main__:main"]
+      },
+
+      install_requires=["tweepy>=3.5"],
+
+      license="BSD3",
+
+      name="fantasy_metro_bot",
+
+      packages=find_packages(),
+
+      # Project"s main homepage
+      url="https://github.com/andrewmichaud/fantasymetro_bot",
+
+      version=VERSION)
