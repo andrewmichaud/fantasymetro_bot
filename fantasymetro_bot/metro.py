383011e111d030ce04623a80dac4c79e62d4daf1
Andrew Michaud <dev@mail.andrewmichaud.com>
2016-11-17 02:12:22 -0800

    Basics of drawing.
    Add code to draw Routes.
    Add BART YAML file and start mapping it to an image.
    Add Bay Area image.

--- fantasymetro_bot/metro.py
+++ fantasymetro_bot/metro.py
@@ -1,5 +1,10 @@
 """Module for a Metro, a set of Routes."""
+import os
+import sys
 import random
+from shutil import copyfile
+
+from PIL import Image, ImageDraw
 
 import route
 import stationset
@@ -20,9 +25,54 @@ class Metro(object):
 
         return out
 
+    def draw(self, filename):
+        """Draw metro onto image."""
+
+        base, ext = os.path.splitext(filename)
+        draw_copy = base + "-drawn" + ext
+        copyfile(filename, draw_copy)
+
+        im = Image.open(draw_copy)
+
+        width, height = im.size
+        print("Image is wxh {}x{}".format(width, height))
+
+        draw = ImageDraw.Draw(im)
+
+        for r in self.routes:
+            # Draw start.
+            RADIUS = 5
+            draw.ellipse([r.start.x - RADIUS, r.start.y - RADIUS,
+                          r.start.x + RADIUS, r.start.y + RADIUS],
+                         outline=r.color)
+            draw.text((r.start.x, r.start.y), r.start.name, fill="black")
+            last = (r.start.x, r.start.y)
+
+            for s in r.stations[1:-1]:
+                draw.line([last, (s.x, s.y)], fill=r.color, width=3)
+                draw.ellipse([s.x - RADIUS, s.y - RADIUS,
+                              s.x + RADIUS, s.y + RADIUS],
+                             outline=r.color, fill=r.color)
+
+                draw.text((s.x, s.y), s.name, fill="black")
+
+                last = (s.x, s.y)
+
+            draw.line([last, (r.end.x, r.end.y)], fill=r.color, width=3)
+            draw.ellipse([r.end.x - RADIUS, r.end.y - RADIUS,
+                          r.end.x + RADIUS, r.end.y + RADIUS],
+                         outline=r.color)
+
+            draw.text((r.end.x, r.end.y), r.end.name, fill="black")
+
+            # draw.line((0, 0) + im.size, fill="red", width=3)
+
+        # write to stdout
+        im.save(draw_copy)
+
 
 if __name__ == "__main__":
-    station_set = stationset.load("sfmetro.yaml")
+    station_set = stationset.load("bayarea.yaml")
 
     NUM_ROUTES_RANGE = range(3, 7)
 
@@ -34,3 +84,5 @@ if __name__ == "__main__":
 
     metro = Metro(routes=routes, name=station_set.name)
     print(repr(metro))
+
+    metro.draw("bayarea.png")
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(author="Andrew Michaud",
           "console_scripts": ["fantasy_metro_bot = fantasy_metro_bot.__main__:main"]
       },
 
-      install_requires=["tweepy>=3.5", "pyyaml>=3.12"],
+      install_requires=["pillow>=3.4.2", "pyyaml>=3.12""tweepy>=3.5"],
 
       license="BSD3",
 
